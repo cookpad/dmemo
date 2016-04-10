bb4c2255969ff5d95ed0ae01d073fb44c91712cd
hogelog <konbu.komuro@gmail.com>
2016-04-10 23:16:27 +0900

    Dynamic access from data_sources table

--- app/models/data_source.rb
+++ app/models/data_source.rb
@@ -1,12 +1,33 @@
 class DataSource < ActiveRecord::Base
-  def source_connection
-    @source_connection ||= ActiveRecord::Base.establish_connection(
+
+  module DynamicTable
+  end
+
+  def source_db_module
+    return @source_db_module if @source_db_module
+
+    @source_db_module = Module.new
+    DynamicTable.const_set(dbname.classify, @source_db_module)
+  end
+
+  def source_base_class
+    return @source_base_class if @source_base_class
+
+    @source_base_class = Class.new(ActiveRecord::Base)
+    source_db_module.const_set("Base", @source_base_class)
+    @source_base_class.establish_connection(
       adapter: adapter,
       host: host,
       port: port,
       username: user,
       password: password,
-      database: database,
+      database: dbname,
     )
+  end
+
+  def tables
+    source_base_class.connection.tables.map do |name|
+      DataSource::Table.new(source_base_class, source_db_module, name)
+    end
   end
 end
