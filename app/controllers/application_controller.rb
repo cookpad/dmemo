99408812680d69f3da8295ad28adf413d4e96552
komuro.sunao <komuro.sunao@mwed.co.jp>
2016-04-14 13:38:10 +0900

    Show database memo in sidebar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :set_database_memos
+
+  private
+
+  def set_database_memos
+    @database_memos = DatabaseMemo.all
+  end
 end
