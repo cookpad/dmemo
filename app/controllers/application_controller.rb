54e5391e2f0688b609b2ab90148e6a435fa76dca
komuro.sunao <komuro.sunao@mwed.co.jp>
2016-04-14 18:43:24 +0900

    Show striped tables

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :set_data_sources
   before_action :set_database_memos
 
   private
+
+  def set_data_sources
+    @data_sources = DataSource.all
+  end
 
   def set_database_memos
     @database_memos = DatabaseMemo.all
