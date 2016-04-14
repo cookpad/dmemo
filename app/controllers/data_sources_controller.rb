8c4caa3bd89d8121ad3785a55fe475f5a9d89317
hogelog <konbu.komuro@gmail.com>
2016-04-14 23:58:47 +0900

    Add data_sources controller

---
+++ app/controllers/data_sources_controller.rb
@@ -0,0 +1,30 @@
+class DataSourcesController < ApplicationController
+  def index
+    @data_sources = DataSource.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+    @data_source = DataSource.find(params[:id])
+  end
+
+  def update
+    data_source = DataSource.find(params[:id])
+    data_source.update!(data_source_params)
+    redirect_to edit_data_source_path(data_source)
+  end
+
+  def destroy
+  end
+
+  private
+
+  def data_source_params
+    params.require(:data_source).permit(:name, :description, :adapter, :host, :port, :dbname, :user, :password, :encoding)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "top#show"
 
+  resources :data_sources, except: %w(show)
+
   resources :database_memos, only: %w(index show create) do
     resources :table_memos, only: %w(index show)
   end
