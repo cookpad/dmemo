1425b6ffd075a33c06932649ba23c810e50bde85
hogelog <konbu.komuro@gmail.com>
2016-04-16 23:52:47 +0900

    Show linked or not

--- app/models/column_memo.rb
+++ app/models/column_memo.rb
@@ -1,3 +1,11 @@
 class ColumnMemo < ActiveRecord::Base
   belongs_to :table_memo
+
+  def source_column_class
+    table_memo.source_column_class(name)
+  end
+
+  def linked?
+    source_column_class.present?
+  end
 end
--- app/models/table_memo.rb
+++ app/models/table_memo.rb
@@ -2,4 +2,16 @@ class TableMemo < ActiveRecord::Base
   belongs_to :database_memo
 
   has_many :column_memos
+
+  def source_table_class
+    database_memo.data_source.try(:source_table_class, name)
+  end
+
+  def source_column_class(column_name)
+    source_table_class.try {|table_class| table_class.columns.find {|column_class| column_class.name == column_name } }
+  end
+
+  def linked?
+    source_table_class
+  end
 end
