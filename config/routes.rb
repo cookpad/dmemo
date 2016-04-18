4687813372331cf2ee787be351037dee8e6a2bfa
hogelog <konbu.komuro@gmail.com>
2016-04-18 23:01:16 +0900

    Use shallow url for table_memos

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :data_sources, except: %w(show)
 
   resources :database_memos, only: %w(index show create destroy) do
-    resources :table_memos, only: %w(index show destroy update)
+    resources :table_memos, only: %w(index show destroy update), shallow: true
   end
 end
