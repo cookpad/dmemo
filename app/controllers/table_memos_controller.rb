104e025ca8701ab61401317a0bacbbc00a9912f4
komuro.sunao <komuro.sunao@mwed.co.jp>
2016-04-18 11:02:40 +0900

    Cache source table data

--- app/controllers/table_memos_controller.rb
+++ app/controllers/table_memos_controller.rb
@@ -8,9 +8,17 @@ class TableMemosController < ApplicationController
     source_table_class = @table_memo.source_table_class
     if source_table_class
       @source_column_classes = source_table_class.columns
-      column_names = @source_column_classes.map(&:name)
-      @source_table_data = source_table_class.limit(20).pluck(*column_names)
+      @source_table_data = fetch_source_table_data(source_table_class, @source_column_classes)
       @source_table_count = source_table_class.count
+    end
+  end
+
+  private
+
+  def fetch_source_table_data(source_table_class, source_column_classes)
+    cache("source_table_data_#{source_table_class.name.underscore}", expire: 1.day) do
+      column_names = source_column_classes.map(&:name)
+      source_table_class.limit(20).pluck(*column_names)
     end
   end
 end
