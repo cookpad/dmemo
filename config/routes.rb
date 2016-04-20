cee4621a66a0f8de7c3538fc721d0d506026baac
komuro.sunao <komuro.sunao@mwed.co.jp>
2016-04-20 15:50:59 +0900

    Revert "Remove import action"
    This reverts commit fb3fa3c910995b3128cec185eba1b8eb0350811e.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :data_sources, except: %w(show)
 
-  resources :database_memos, only: %w(index show update destroy) do
+  resources :database_memos, only: %w(index show create update destroy) do
     resources :table_memos, only: %w(index show update destroy), shallow: true do
       resources :column_memos, only: %w(update destroy)
     end
