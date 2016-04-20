f9414991a5ed095c57a1279b9807e899240a2d6e
komuro.sunao <komuro.sunao@mwed.co.jp>
2016-04-20 15:50:53 +0900

    Revert "Load data source automatically"
    This reverts commit 90ffa910e6bdda405a3ef41a10bb19a69c2c5924.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :reload_data_sources
   before_action :set_database_memos
 
   private
-
-  def reload_data_sources
-    DataSource.find_each do |data_source|
-      DatabaseMemo.import_data_source!(data_source.id)
-      data_source.reset_source_table_classes!
-    end
-  end
 
   def set_database_memos
     @database_memos = DatabaseMemo.all.includes(:data_source, :table_memos)
