5597903f23b1a0017b9c39a3be888c1165b02cb9
hogelog <konbu.komuro@gmail.com>
2016-04-21 22:54:16 +0900

    Masked database

--- app/models/masked_data.rb
+++ app/models/masked_data.rb
@@ -7,11 +7,16 @@ class MaskedData
     end
   end
 
+  def self.masked_database_names
+    Array.wrap(data["databases"])
+  end
+
   def self.masked_table_names
     Array.wrap(data["tables"])
   end
 
   def self.masked_table?(database_name, table_name)
+    return true if masked_database_names.include?(database_name)
     formal_table_name = "#{database_name}/#{table_name}"
     masked_table_names.any? {|masked_table_name| [formal_table_name, table_name].include?(masked_table_name) }
   end
