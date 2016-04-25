2be60b0a32003e9eca55f6b117c6e0c9f9abcab8
hogelog <konbu.komuro@gmail.com>
2016-04-26 00:07:16 +0900

    Add md2text pipeline

--- app/decorators/markdown_description_decorator.rb
+++ app/decorators/markdown_description_decorator.rb
@@ -1,5 +1,9 @@
 module MarkdownDescriptionDecorator
   def description_html
-    @description_html ||= Rails.application.config.html_pipeline.call(description)[:output].html_safe
+    @description_html ||= Rails.application.config.markdown_to_html_pipeline.call(description)[:output].html_safe
+  end
+
+  def description_text
+    @description_text ||= Rails.application.config.markdown_to_text_pipeline.call(description)[:output].html_safe
   end
 end
--- config/initializers/html_pipeline.rb
+++ config/initializers/html_pipeline.rb
@@ -1,7 +1,18 @@
 require 'html/pipeline'
 
-Rails.application.config.html_pipeline = HTML::Pipeline.new [
+Rails.application.config.markdown_to_html_pipeline = HTML::Pipeline.new [
   HTML::Pipeline::MarkdownFilter,
   HTML::Pipeline::RougeFilter,
-  HTML::Pipeline::AutolinkFilter
+  HTML::Pipeline::AutolinkFilter,
+]
+
+class InnerTextFilter < HTML::Pipeline::Filter
+  def call
+    doc.text
+  end
+end
+
+Rails.application.config.markdown_to_text_pipeline = HTML::Pipeline.new [
+  HTML::Pipeline::MarkdownFilter,
+  HTML::Pipeline::InnerTextFilter,
 ]
