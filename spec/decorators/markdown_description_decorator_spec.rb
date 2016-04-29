d98024083738c7cf3fe0329863345136d833457b
hogelog <konbu.komuro@gmail.com>
2016-04-29 22:47:59 +0900

    Remove unused spec

--- spec/decorators/markdown_description_decorator_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-describe MarkdownDescriptionDecorator do
-  let(:markdown_description) { MarkdownDescription.new.extend MarkdownDescriptionDecorator }
-  subject { markdown_description }
-  it { should be_a MarkdownDescription }
-end
