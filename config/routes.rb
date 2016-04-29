049e0a2f3f846fa8cd28021ba2b0299dd5386731
hogelog <konbu.komuro@gmail.com>
2016-04-29 22:47:51 +0900

    Friendly url

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
 
   resources :data_sources, except: %w(show)
 
-  resources :database_memos, only: %w(index show create update destroy) do
-    resources :table_memos, only: %w(index show update destroy), shallow: true do
-      resources :column_memos, only: %w(update destroy)
+  resources :databases, controller: :database_memos, as: :database_memos, only: %w(index show create update destroy) do
+    resources :tables, controller: :table_memos, as: :table_memos, only: %w(index show update destroy), shallow: true do
+      resources :columns, controller: :column_memos, as: :column_memos, only: %w(update destroy)
     end
   end
 
