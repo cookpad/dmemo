c6944a3606bd5f0c342660d16cc86eb95e75d099
hogelog <konbu.komuro@gmail.com>
2016-05-24 21:54:39 +0900

    Fix omniauth config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  if ENV["GOOGLE_HOSTED_DOMAIN"].present?
-    provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], hd: ENV["GOOGLE_HOSTED_DOMAIN"]
-  else
-    provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
-  end
+  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {
+    hd: ENV["GOOGLE_HOSTED_DOMAIN"].present? ? ENV["GOOGLE_HOSTED_DOMAIN"] : nil,
+  }
 end
