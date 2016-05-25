698c4c1f81ec46eba94ea9a270364f5663efd221
hogelog <konbu.komuro@gmail.com>
2016-05-25 22:44:19 +0900

    Import data source after create

--- app/controllers/data_sources_controller.rb
+++ app/controllers/data_sources_controller.rb
@@ -12,8 +12,8 @@ class DataSourcesController < ApplicationController
   end
 
   def create(data_source)
-    DataSource.create!(data_source_params(data_source))
-    redirect_to data_sources_path
+    @data_source = DataSource.create!(data_source_params(data_source))
+    import_data_source_and_redirect!(@data_source)
   end
 
   def edit(id)
@@ -26,13 +26,7 @@ class DataSourcesController < ApplicationController
     @data_source.reset_source_table_classes!
     @data_source.assign_attributes(data_source_params(data_source))
     @data_source.save! if @data_source.changed?
-    DatabaseMemo.import_data_source!(@data_source.id)
-    flash[:updated] = t("data_source_updated", name: @data_source.name)
-    redirect_to data_sources_path
-  rescue ActiveRecord::ActiveRecordError => e
-  rescue Mysql2::Error => e
-    flash[:error] = e.message
-    redirect_to data_sources_path
+    import_data_source_and_redirect!(@data_source)
   end
 
   def destroy(id)
@@ -46,5 +40,14 @@ class DataSourcesController < ApplicationController
   def data_source_params(data_source)
     data_source.reject!{|k, v| k == "password" && v == DUMMY_PASSWORD }
     data_source
+  end
+
+  def import_data_source_and_redirect!(data_source)
+    DatabaseMemo.import_data_source!(data_source.id)
+    flash[:updated] = t("data_source_updated", name: data_source.name)
+    redirect_to data_sources_path
+  rescue ActiveRecord::ActiveRecordError, Mysql2::Error => e
+    flash[:error] = e.message
+    redirect_to data_sources_path
   end
 end
