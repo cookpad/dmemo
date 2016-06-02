f09ca88ef1aeb4bacb2636e24572cdc1a641661d
hogelog <konbu.komuro@gmail.com>
2016-06-03 00:26:46 +0900

    hoge

---
+++ app/models/masked_datum.rb
@@ -0,0 +1,45 @@
+class MaskedDatum < ActiveRecord::Base
+  def self.data
+    @@data = {
+      databases: where.not(database: nil)
+    }
+  end
+
+  def self.masked_database_names
+    Array.wrap(data[:databases])
+  end
+
+  def self.masked_table_names
+    Array.wrap(data[:tables])
+  end
+
+  def self.masked_column_names
+    Array.wrap(data[:columns])
+  end
+
+  def self.masked_database?(database_name)
+    masked_database_names.include?(database_name)
+  end
+
+  def self.masked_table?(database_name, table_name)
+    return true if masked_database?(database_name)
+    masked_table_names.any? do |masked_table_name|
+      [
+        "#{database_name}/#{table_name}",
+        table_name
+      ].include?(masked_table_name)
+    end
+  end
+
+  def self.masked_column?(database_name, table_name, column_name)
+    return true if masked_database?(database_name)
+    return true if masked_table?(database_name, table_name)
+    masked_column_names.any? do |masked_column_name|
+      [
+        "#{database_name}/#{table_name}/#{column_name}",
+        "#{table_name}/#{column_name}",
+        column_name
+      ].include?(masked_column_name)
+    end
+  end
+end
