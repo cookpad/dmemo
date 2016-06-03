7f546cb7e46b248bd83fc96ef2b68992d2d1a43f
hogelog <konbu.komuro@gmail.com>
2016-06-04 00:37:08 +0900

    Remove MaskedData

--- app/models/masked_datum.rb
+++ app/models/masked_datum.rb
@@ -1,29 +1,17 @@
 class MaskedDatum < ActiveRecord::Base
+  after_save :update_data!
+
   def self.data
-    @@data = {
-      databases: where.not(database: nil)
-    }
-  end
-
-  def self.masked_database_names
-    Array.wrap(data[:databases])
-  end
-
-  def self.masked_table_names
-    Array.wrap(data[:tables])
-  end
-
-  def self.masked_column_names
-    Array.wrap(data[:columns])
+    @@data ||= update_data!
   end
 
   def self.masked_database?(database_name)
-    masked_database_names.include?(database_name)
+    data[:database].include?(database_name)
   end
 
   def self.masked_table?(database_name, table_name)
     return true if masked_database?(database_name)
-    masked_table_names.any? do |masked_table_name|
+    data[:table].any? do |masked_table_name|
       [
         "#{database_name}/#{table_name}",
         table_name
@@ -34,12 +22,39 @@ class MaskedDatum < ActiveRecord::Base
   def self.masked_column?(database_name, table_name, column_name)
     return true if masked_database?(database_name)
     return true if masked_table?(database_name, table_name)
-    masked_column_names.any? do |masked_column_name|
+    data[:column].any? do |masked_column_name|
       [
         "#{database_name}/#{table_name}/#{column_name}",
         "#{table_name}/#{column_name}",
         column_name
       ].include?(masked_column_name)
     end
+  end
+
+  private
+
+  def self.update_data!
+    data = { database: [], table: [], column: [] }
+    all.find_each do |datum|
+      case [datum.database_name?, datum.table_name?, datum.column_name?]
+        when [false, false, true]
+          data[:column] << datum.column_name
+        when [false, true, true]
+          data[:column] << "#{datum.table_name}/#{datum.column_name}"
+        when [true, true, true]
+          data[:column] << "#{datum.database_name}/#{datum.table_name}/#{datum.column_name}"
+        when [false, true, false]
+          data[:table] << datum.table_name
+        when [true, true, false]
+          data[:table] << "#{datum.database_name}/#{datum.table_name}"
+        when [true, false, false]
+          data[:database] << datum.database_name
+      end
+    end
+    @@data = data
+  end
+
+  def update_data!
+    self.class.update_data!
   end
 end
