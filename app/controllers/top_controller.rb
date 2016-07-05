405604e0aa31e0f6964c8df2f95cfd430aefd489
hogelog <konbu.komuro@gmail.com>
2016-07-05 23:15:06 +0900

    Fix deleted favorite table

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,6 @@
 class TopController < ApplicationController
   def show
     @database_memos = DatabaseMemo.all.includes(:data_source, schema_memos: :table_memos).sort_by(&:display_order)
-    @favorite_tables = current_user.favorite_tables.includes(:table_memo)
+    @favorite_tables = TableMemo.where(id: current_user.favorite_tables.pluck(:table_memo_id))
   end
 end
