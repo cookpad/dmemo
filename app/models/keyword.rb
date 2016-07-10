04eb1e6c3e88cf14456f4ca955033c432929488c
hogelog <konbu.komuro@gmail.com>
2016-07-10 22:57:01 +0900

    Clear keyword links cache

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,8 +1,11 @@
 class Keyword
   def self.links
-    # TODO: Clear cache.
     @links ||= TableMemo.distinct(:name).pluck(:name).each_with_object({}) {|name, h|
       h[name] = Rails.application.routes.url_helpers.keyword_path(name)
     }
+  end
+
+  def self.clear_links!
+    @links = nil
   end
 end
