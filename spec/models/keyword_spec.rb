912d466e5ca601ba4fa6d1d25e56c6a4ac4806e3
hogelog <konbu.komuro@gmail.com>
2016-07-11 23:21:39 +0900

    Autolink to database_schema_table_path

--- spec/models/keyword_spec.rb
+++ spec/models/keyword_spec.rb
@@ -2,32 +2,44 @@ require "rails_helper"
 
 describe Keyword, type: :model do
   describe ".links" do
+    let(:database_memo) { FactoryGirl.create(:database_memo, name: "db") }
+    let(:schema_memo) { FactoryGirl.create(:schema_memo, database_memo: database_memo, name: "myapp") }
     before do
-      SchemaMemo.create!(id: 1, name: "myapp", database_memo_id: 0)
-      TableMemo.create!(id: 97, name: "books", schema_memo_id: 1)
+      FactoryGirl.create(:table_memo, schema_memo: schema_memo, name: "books")
     end
 
-    it { expect(Keyword.links).to eq("books" => "/keywords/books", "myapp.books" => "/tables/97") }
+    it "returns links to table" do
+      expect(Keyword.links).to eq(
+        "books" => "/databases/db/myapp/books",
+        "myapp.books" => "/databases/db/myapp/books",
+      )
+    end
 
     context "after table_memos created" do
       it "clears links" do
-        expect(Keyword.links).to eq("books" => "/keywords/books", "myapp.books" => "/tables/97")
-        memo = TableMemo.create!(id: 98, name: "blogs", schema_memo_id: 1)
         expect(Keyword.links).to eq(
-          "blogs" => "/keywords/blogs",
-          "myapp.blogs" => "/tables/98",
-          "books" => "/keywords/books",
-          "myapp.books" => "/tables/97",
+          "books" => "/databases/db/myapp/books",
+          "myapp.books" => "/databases/db/myapp/books",
+        )
+        memo = FactoryGirl.create(:table_memo, schema_memo: schema_memo, name: "blogs")
+        expect(Keyword.links).to eq(
+          "books" => "/databases/db/myapp/books",
+          "myapp.books" => "/databases/db/myapp/books",
+          "blogs" => "/databases/db/myapp/blogs",
+          "myapp.blogs" => "/databases/db/myapp/blogs",
         )
         memo.update!(name: "alphabets")
         expect(Keyword.links).to eq(
-          "alphabets" => "/keywords/alphabets",
-          "myapp.alphabets" => "/tables/98",
-          "books" => "/keywords/books",
-          "myapp.books" => "/tables/97",
+          "books" => "/databases/db/myapp/books",
+          "myapp.books" => "/databases/db/myapp/books",
+          "alphabets" => "/databases/db/myapp/alphabets",
+          "myapp.alphabets" => "/databases/db/myapp/alphabets",
         )
         memo.destroy!
-        expect(Keyword.links).to eq("books" => "/keywords/books", "myapp.books" => "/tables/97")
+        expect(Keyword.links).to eq(
+          "books" => "/databases/db/myapp/books",
+          "myapp.books" => "/databases/db/myapp/books",
+        )
       end
     end
   end
