2faa6466fef70abea13c90780e3ea8c7a2c8c782
hogelog <konbu.komuro@gmail.com>
2016-07-14 01:04:53 +0900

    Autolink keywords

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -4,6 +4,14 @@ class Keyword < ActiveRecord::Base
 
   has_many :logs, -> { order(:id) }, class_name: "KeywordLog"
 
-
   validates :name, :description, presence: true
+
+  after_save :clear_keyword_links
+  after_destroy :clear_keyword_links
+
+  private
+
+  def clear_keyword_links
+    AutolinkKeyword.clear_links! if name_changed? || destroyed?
+  end
 end
---
+++ spec/factories/keyword_factory.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :keyword do
+    name "keyword"
+    description "# keyword description"
+  end
+end
