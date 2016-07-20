e0770a9ac2a61232469bde276170fe3e6b280051
hogelog <konbu.komuro@gmail.com>
2016-07-20 23:45:32 +0900

    Remove unused method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,4 @@
 module ApplicationHelper
-  def link_to_editable(obj, data_options)
-    opts = data_options.reverse_merge(pk: obj.id, url: url_for(obj))
-    class_value = (%w(editable-field) << data_options[:class]).compact
-    content_tag :a, class: class_value, data: opts do
-      content_tag :i, "", class: "fa fa-edit"
-    end
-  end
-
   def render_diff(diff)
     Markdown.new(<<-DIFF).html
 ```diff
