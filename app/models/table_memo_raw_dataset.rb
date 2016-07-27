3a2c9c25e6e877ccc8afea03b3c6b0387825b152
hogelog <konbu.komuro@gmail.com>
2016-07-27 23:29:11 +0900

    Revert "Load table raw data when Databasememo.import_data_source!"

--- app/models/table_memo_raw_dataset.rb
+++
@@ -1,6 +0,0 @@
-class TableMemoRawDataset < ActiveRecord::Base
-  belongs_to :table_memo
-
-  has_many :columns, class_name: "TableMemoRawDatasetColumn", dependent: :destroy
-  has_many :rows, class_name: "TableMemoRawDatasetRow", dependent: :destroy
-end
--- app/models/table_memo_raw_dataset_row.rb
+++
@@ -1,3 +0,0 @@
-class TableMemoRawDatasetRow < ActiveRecord::Base
-  belongs_to :table_memo_raw_dataset, class_name: "TableMemoRawDataset"
-end
