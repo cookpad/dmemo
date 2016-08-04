69b587a1f9c5c691f80d16fd670010ebb2bfb0d2
Minero Aoki <mineroaoki@gmail.com>
2016-08-04 12:02:13 +0900

    Should not show timezone for timestamp without time zone values.

--- app/models/table_memo_raw_dataset_column.rb
+++ app/models/table_memo_raw_dataset_column.rb
@@ -1,3 +1,15 @@
 class TableMemoRawDatasetColumn < ActiveRecord::Base
   belongs_to :table_memo_raw_dataset, class_name: "TableMemoRawDataset"
+
+  def format_value(value)
+    case sql_type
+    when 'timestamp without time zone'
+      d, t, z = value.split
+      "#{d} #{t}"
+    when /timestamp/
+      value
+    else
+      value.to_s
+    end
+  end
 end
