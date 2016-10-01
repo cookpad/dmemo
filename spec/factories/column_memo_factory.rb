b8016cb50e8bc11170a6b2013026967057b06df6
hogelog <konbu.komuro@gmail.com>
2016-10-01 23:43:58 +0900

    Add column_memos spec

---
+++ spec/factories/column_memo_factory.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :column_memo do
+    table_memo
+    sequence(:name) {|n| "column#{n}" }
+    description "# column memo"
+  end
+end
---
+++ spec/requests/column_memos_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+describe :column_memos, type: :request do
+  let(:column_memo) { FactoryGirl.create(:column_memo) }
+  let(:table_memo) { column_memo.table_memo }
+  let(:schema_memo) { table_memo.schema_memo }
+  let(:database_memo) { schema_memo.database_memo }
+  before do
+    login!
+  end
+
+  describe "#edit" do
+    it "shows form" do
+      get edit_column_memo_path(column_memo)
+      expect(response).to render_template("column_memos/edit")
+      expect(page).to have_content(column_memo.description)
+    end
+  end
+
+  describe "#update" do
+    it "updates memo" do
+      patch column_memo_path(column_memo), column_memo: { description: "foo description" }
+      expect(response).to redirect_to(redirect_to database_schema_table_path(database_memo.name, schema_memo.name, table_memo.name))
+      expect(assigns(:column_memo).description).to eq("foo description")
+    end
+  end
+
+  describe "#destroy" do
+    it "destroys memo" do
+      delete column_memo_path(column_memo)
+      expect(response).to redirect_to(table_memo_path(column_memo.table_memo_id))
+      expect { ColumnMemo.find(column_memo.id) }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
+end
