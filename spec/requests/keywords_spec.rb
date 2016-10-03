ce9ddec08d9d6c4f364c81024b4d8dbcb7d60d77
hogelog <konbu.komuro@gmail.com>
2016-10-03 23:55:34 +0900

    More keywords spec

--- spec/requests/keywords_spec.rb
+++ spec/requests/keywords_spec.rb
@@ -38,4 +38,54 @@ describe :keywords, type: :request do
       expect(page).to have_xpath("//strong[text()='delicious']")
     end
   end
+
+  describe "#new" do
+    it "shows new page" do
+      get new_keyword_path
+      expect(response).to render_template("keywords/new")
+    end
+  end
+
+  describe "#create" do
+    it "creates keyword" do
+      post keywords_path, keyword: { name: "foo", description: "foo description" }
+      keyword = Keyword.find_by!(name: "foo")
+      expect(response).to redirect_to(keyword)
+      expect(keyword.logs.count).to eq(1)
+    end
+  end
+
+  describe "#edit" do
+    let(:keyword) { FactoryGirl.create(:keyword, name: "sushi", description: "**delicious**") }
+
+    it "shows edit form" do
+      get edit_keyword_path(keyword)
+      expect(response).to render_template("keywords/edit")
+      expect(page).to have_text("sushi")
+      expect(page).to have_text("delicious")
+    end
+  end
+
+  describe "#update" do
+    let(:keyword) { FactoryGirl.create(:keyword, name: "sushi", description: "sushi 1") }
+
+    it "updates keyword" do
+      expect(keyword.logs.count).to eq(0)
+      patch keyword_path(keyword), keyword: { description: "sushi 2" }
+      expect(response).to redirect_to(keyword)
+      expect(keyword.reload.description).to eq("sushi 2")
+      expect(keyword.logs.count).to eq(1)
+    end
+  end
+
+  describe "#destroy" do
+    let(:keyword) { FactoryGirl.create(:keyword, name: "sushi", description: "sushi 1") }
+
+    it "destroys keyword" do
+      delete keyword_path(keyword)
+      expect(response).to redirect_to(keywords_path)
+      expect(Keyword.count).to eq(0)
+      expect(KeywordLog.count).to eq(0)
+    end
+  end
 end
