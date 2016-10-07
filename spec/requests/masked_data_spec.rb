6684b0453837377ebd706010a2d826e0519a7e34
hogelog <konbu.komuro@gmail.com>
2016-10-07 23:42:12 +0900

    Add masked data spec

---
+++ spec/requests/masked_data_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+describe :masked_data, type: :request do
+  let(:user) { FactoryGirl.create(:user, admin: true) }
+  before do
+    login!(user: user)
+  end
+
+  describe "#index" do
+    it "redirects" do
+      get masked_data_path
+      expect(response).to redirect_to(setting_path)
+    end
+  end
+
+  describe "#new" do
+    let!(:database_memo) { FactoryGirl.create(:database_memo, name: "foo") }
+
+    it "shows" do
+      get new_masked_datum_path
+      expect(response).to render_template("masked_data/new")
+      expect(assigns(:database_name_options)).to eq([["*", "*"], ["foo", "foo"]])
+    end
+  end
+end
