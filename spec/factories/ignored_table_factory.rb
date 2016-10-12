7ee0dbea6e5c00f1b909e6513454e697be4027ea
hogelog <konbu.komuro@gmail.com>
2016-10-12 23:53:09 +0900

    Add ignored_tables spec

---
+++ spec/factories/ignored_table_factory.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :ignored_table do
+    data_source
+    pattern ".+_(?:old|wk|l)"
+  end
+end
---
+++ spec/requests/ignored_tables_spec.rb
@@ -0,0 +1,62 @@
+require "rails_helper"
+
+describe :ignored_table, type: :request do
+  let(:user) { FactoryGirl.create(:user, admin: true) }
+  before do
+    login!(user: user)
+  end
+
+  describe "#index" do
+    it "redirects" do
+      get ignored_tables_path
+      expect(response).to redirect_to(setting_path)
+    end
+  end
+
+  describe "#show" do
+    let!(:ignored_table) { FactoryGirl.create(:ignored_table) }
+
+    it "redirects" do
+      get ignored_table_path(ignored_table)
+      expect(response).to redirect_to(setting_path)
+    end
+  end
+
+  describe "#new" do
+    let!(:data_source) { FactoryGirl.create(:data_source) }
+
+    it "shows form" do
+      get new_ignored_table_path
+      expect(response).to render_template("ignored_tables/new")
+      expect(page).to have_xpath("//option[text()='#{data_source.name}']")
+    end
+  end
+
+  describe "#create" do
+    let(:data_source) { FactoryGirl.create(:data_source) }
+
+    it "creates ignored_table" do
+      post ignored_tables_path, ignored_table: { data_source_id: data_source.id, pattern: "foo" }
+      expect(response).to redirect_to(setting_path)
+      expect(assigns(:ignored_table).pattern).to eq("foo")
+    end
+
+    context "with empty pattern" do
+      it "shows error" do
+        post ignored_tables_path, ignored_table: { data_source_id: data_source.id, pattern: "" }
+        expect(response).to redirect_to(new_ignored_table_path)
+        expect(flash[:error]).to include("Pattern")
+      end
+    end
+  end
+
+  describe "#destroy" do
+    let(:ignored_table) { FactoryGirl.create(:ignored_table) }
+
+    it "creates ignored_table" do
+      delete ignored_table_path(ignored_table)
+      expect(response).to redirect_to(setting_path)
+      expect(IgnoredTable.find_by(id: ignored_table.id)).to eq(nil)
+    end
+  end
+end
