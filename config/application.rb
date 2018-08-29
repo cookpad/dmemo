3c77311ff3023700803172f1cd92012741ce2148
kyoh.maruo <romio_cinderella@yahoo.co.jp>
2018-08-29 15:02:09 +0900

    Apply rails 5.2 default settings except `belongs_to_required_by_default`

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Dmemo
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.load_defaults 5.2
+    config.active_record.belongs_to_required_by_default = false
 
     config.autoload_paths << "#{Rails.root}/lib/autoload"
   end
