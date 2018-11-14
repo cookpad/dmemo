eefe9bad4bc17ccd48d1d75d527563d8b67763d6
kyoh.maruo <romio_cinderella@yahoo.co.jp>
2018-11-14 17:33:18 +0900

    Introduce presto_adapter

--- config/initializers/data_source_adapters.rb
+++ config/initializers/data_source_adapters.rb
@@ -3,4 +3,5 @@ ActiveSupport.on_load(:active_record) do
   DataSourceAdapters.register_adapter(DataSourceAdapters::Mysql2Adapter, 'mysql2')
   DataSourceAdapters.register_adapter(DataSourceAdapters::RedshiftAdapter, 'redshift')
   DataSourceAdapters.register_adapter(DataSourceAdapters::BigqueryAdapter, 'bigquery')
+  DataSourceAdapters.register_adapter(DataSourceAdapters::PrestoAdapter, 'presto')
 end
