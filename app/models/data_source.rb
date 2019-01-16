015e9c73b3808999a84c6f5e5fba688a7b9c2a59
kyoh.maruo <romio_cinderella@yahoo.co.jp>
2019-01-16 15:00:57 +0900

    ignore schema pattern with ignored_tables

--- app/models/data_source.rb
+++ app/models/data_source.rb
@@ -12,11 +12,11 @@ class DataSource < ApplicationRecord
 
   def source_table_names
     table_names = access_logging { data_source_adapter.fetch_table_names }
-    table_names.reject { |_, table_name| ignored_table_patterns.match(table_name) }
+    table_names.reject { |schema_name, table_name| ignore(schema_name, table_name) }
   end
 
   def data_source_table(schema_name, table_name, table_names)
-    return if ignored_table_patterns.match(table_name)
+    return if ignore(schema_name, table_name)
     schema_name, _ = table_names.find {|schema, table| schema == schema_name && table == table_name }
     return nil unless schema_name
     DataSourceTable.new(self, schema_name, table_name)
@@ -29,10 +29,6 @@ class DataSource < ApplicationRecord
     end
   end
 
-  def ignored_table_patterns
-    @ignored_table_patterns ||= Regexp.union(ignored_tables.pluck(:pattern).map {|pattern| Regexp.new(pattern, true) })
-  end
-
   def access_logging
     Rails.logger.tagged("DataSource #{name}") { yield }
   end
@@ -43,5 +39,15 @@ class DataSource < ApplicationRecord
 
   def standard_adapter?
     adapter.in? DataSourceAdapters.standard_adapter_names
+  end
+
+  private
+
+  def ignore(schema_name, table_name)
+    ignored_table_patterns.match("#{schema_name}.#{table_name}")
+  end
+
+  def ignored_table_patterns
+    @ignored_table_patterns ||= Regexp.union(ignored_tables.pluck(:pattern).map { |pattern| Regexp.new(pattern, true) })
   end
 end
