bc231b39cd002bcd1b3aeddc5e6c500072f4c77b
ragi <sukerutulo+github@gmail.com>
2019-03-25 18:04:46 +0900

    Modify redshift adapter to support spectrum

--- app/models/data_source_adapters/redshift_adapter.rb
+++ app/models/data_source_adapters/redshift_adapter.rb
@@ -3,7 +3,7 @@ require "active_record/connection_adapters/redshift_adapter"
 module DataSourceAdapters
   class RedshiftAdapter < StandardAdapter
     def fetch_table_names
-      source_base_class.connection.query(<<~SQL, 'SCHEMA')
+      @table_names = source_base_class.connection.query(<<~SQL, 'SCHEMA')
         SELECT schemaname, tablename
         FROM (
           SELECT schemaname, tablename FROM pg_tables WHERE schemaname = ANY (current_schemas(false))
@@ -12,8 +12,43 @@ module DataSourceAdapters
         ) tables
         ORDER BY schemaname, tablename;
       SQL
+
+      @external_table_names = source_base_class.connection.query(<<~SQL, 'SCHEMA')
+        SELECT schemaname, tablename FROM svv_external_tables ORDER BY schemaname, tablename;
+      SQL
+
+      @table_names + @external_table_names
     rescue ActiveRecord::ActiveRecordError, PG::Error => e
       raise DataSource::ConnectionBad.new(e)
+    end
+
+    def fetch_columns(table)
+      adapter = connection.pool.connection
+      if spectrum?(table)
+        connection.query(<<~SQL, 'COLUMN').map { |name, sql_type| Column.new(name, sql_type, "NULL", true) }
+          SELECT columnname, external_type FROM svv_external_columns WHERE tablename = '#{table.table_name}';
+        SQL
+      else
+        connection.columns(table.full_table_name).map { |c| Column.new(c.name, c.sql_type, adapter.quote(c.default), c.null) }
+      end
+    rescue ActiveRecord::ActiveRecordError, Mysql2::Error, PG::Error => e
+      raise DataSource::ConnectionBad.new(e)
+    end
+
+    def fetch_rows(table, limit)
+      return [] if spectrum?(table)
+      super
+    end
+
+    def fetch_count(table)
+      return 0 if spectrum?(table)
+      super
+    end
+
+    private
+
+    def spectrum?(table)
+      @external_table_names.include?(table.full_table_name.split('.'))
     end
   end
 end
