c2e62c0a0e96ecc6008217e5937097a930bacaa4
kitsuyui <kitsuyui@kitsuyui.com>
2019-05-06 18:29:38 +0900

    Browsing without login by setting environment variable
    - Enable readonly browsing when DMEMO_ALLOW_ANONYMOUS_TO_READ is set
    - Hide favorite tables without login user.
    - Update RSpec for anonymous login

--- spec/requests/top_spec.rb
+++ spec/requests/top_spec.rb
@@ -4,14 +4,34 @@ describe :top, type: :request do
   before do
     FactoryBot.create(:data_source)
     SynchronizeDataSources.run
-    login!
   end
 
   describe "#show" do
-    it "shows top page" do
-      get root_path
-      expect(page).to have_content("DatabaseMEMO")
-      expect(page).to have_selector("a[href='/databases/dmemo']")
+    context "with signed-in" do
+      before { login! }
+      it "shows top page" do
+        get root_path
+        expect(page).to have_content("DatabaseMEMO")
+        expect(page).to have_selector("a[href='/databases/dmemo']")
+      end
+    end
+
+    context "with not signed-in" do
+      context 'with disallowing anonymous to read' do
+        it "redirects" do
+          get root_path
+          expect(response.location).to match('http://www.example.com/auth/google_oauth2.*?')
+        end
+      end
+
+      context 'with allowing anonymous to read' do
+        before { Rails.application.config.allow_anonymous_to_read = true }
+        it "shows top page" do
+          get root_path
+          expect(page).to have_content("DatabaseMEMO")
+          expect(page).to have_selector("a[href='/databases/dmemo']")
+        end
+      end
     end
   end
 end
