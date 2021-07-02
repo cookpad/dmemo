f0f1517172dc53f49466df480c4d0b7869070b04
ragi <sukerutulo+github@gmail.com>
2021-07-02 18:54:31 +0900

    Add description column to search target

--- app/models/search_result.rb
+++ app/models/search_result.rb
@@ -9,7 +9,8 @@ class SearchResult
 
   def search!
     return unless keyword.present?
-    self.results += TableMemo.where("table_memos.name LIKE ?", "%#{keyword}%").eager_load(schema_memo: :database_memo).to_a
-    self.results += ColumnMemo.where("column_memos.name LIKE ?", "%#{keyword}%").eager_load(table_memo: {schema_memo: :database_memo}).to_a
+    pattern = "%#{keyword}%"
+    self.results += TableMemo.where("table_memos.name LIKE ? OR table_memos.description LIKE ?", pattern, pattern).eager_load(schema_memo: :database_memo).to_a
+    self.results += ColumnMemo.where("column_memos.name LIKE ? OR column_memos.description LIKE ?", pattern, pattern).eager_load(table_memo: {schema_memo: :database_memo}).to_a
   end
 end
