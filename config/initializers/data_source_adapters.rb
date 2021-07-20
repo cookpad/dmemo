0b8ec6f0aebf9e6390b34f23e6e621f49cdeec23
ragi <sukerutulo+github@gmail.com>
2021-07-20 17:42:19 +0900

    Load data_source_adapter initializer every access in development environment

--- config/initializers/data_source_adapters.rb
+++ config/initializers/data_source_adapters.rb
@@ -1,7 +1,9 @@
-ActiveSupport.on_load(:active_record) do
-  DataSourceAdapters.register_adapter(DataSourceAdapters::PostgresqlAdapter, 'postgresql')
-  DataSourceAdapters.register_adapter(DataSourceAdapters::Mysql2Adapter, 'mysql2')
-  DataSourceAdapters.register_adapter(DataSourceAdapters::RedshiftAdapter, 'redshift')
-  DataSourceAdapters.register_adapter(DataSourceAdapters::BigqueryAdapter, 'bigquery')
-  DataSourceAdapters.register_adapter(DataSourceAdapters::PrestoAdapter, 'presto')
+Rails.application.config.to_prepare do
+  ActiveSupport.on_load(:active_record) do
+    DataSourceAdapters.register_adapter(DataSourceAdapters::PostgresqlAdapter, 'postgresql')
+    DataSourceAdapters.register_adapter(DataSourceAdapters::Mysql2Adapter, 'mysql2')
+    DataSourceAdapters.register_adapter(DataSourceAdapters::RedshiftAdapter, 'redshift')
+    DataSourceAdapters.register_adapter(DataSourceAdapters::BigqueryAdapter, 'bigquery')
+    DataSourceAdapters.register_adapter(DataSourceAdapters::PrestoAdapter, 'presto')
+  end
 end
