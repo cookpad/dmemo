100d00052957a7e250dee4460921ebc5cd54bec0
ragi <sukerutulo+github@gmail.com>
2021-08-04 21:24:35 +0900

    Modify logger to change output in development env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,9 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  logger           = ActiveSupport::Logger.new(STDOUT)
+  logger.formatter = config.log_formatter
+  config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  config.log_level = :debug
 end
