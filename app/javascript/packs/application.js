07534fbf38dc076c04a32aad2c2b2971349543e1
Takuma Ishikawa <nekketsuuu@cookpad.com>
2024-03-05 14:19:36 +0900

    Update AdminLTE to v3, Bootstrap to v4, jQuery to v3
    Docs:
    
    - https://adminlte.io/docs/3.2/upgrade-guide.html
    - https://getbootstrap.com/docs/4.4/migration/
    - https://jquery.com/upgrade-guide/3.0/
    
    AdminLTE bundles Bootstrap v4, and Bootstrap v4 depends on jQuery v3.
    
    Core points:
    
    - AdminLTE
        - Skin is deleted; so manually write styles.
        - Rewrite header & sidebar in the new manner.
        - Use font-awesome's `fas` instead of `fa-solid` because timeline does not recognize the latter.
    - Bootstrap
        - Use `card` instead of `box`. I think v4's `card` is similar to v3's `box`.
        - Try using `rem` instead of `px` since Bootstrap v4 does so.
        - Flexbox is enabled by default.
        - Add appropriate classes to every `li` element.
    - jQuery
        - Some forms of document-ready handlers are deprecated.
        - .bind() is deprecated.
    
    Some notable style changes:
    
    - Make sidebar be collapsible.
    - Make tables responsible so that we can scroll too-wide tables horizontally.
    - Show icons on edit timelines as AdminLTE's docs do.
    - Fix font of text in deletion warnings for unlinked databases.
    - Delete padding between cards and tables in the cards.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,15 +2,13 @@ import '@fortawesome/fontawesome-free/css/brands.css';
 import '@fortawesome/fontawesome-free/css/fontawesome.css';
 import '@fortawesome/fontawesome-free/css/regular.css';
 import '@fortawesome/fontawesome-free/css/solid.css';
-import 'admin-lte/bootstrap/css/bootstrap.css';
-import 'admin-lte/dist/css/AdminLTE.css';
-import 'admin-lte/dist/css/skins/skin-blue.css';
+import 'admin-lte/dist/css/adminlte.css';
 import 'jquery-colorbox/example4/colorbox.css';
 import 'pygments-css/github.css';
 import '../dmemo.css';
 
-import 'admin-lte/bootstrap/js/bootstrap';
-import 'admin-lte/dist/js/app';
+import 'admin-lte/dist/js/adminlte';
+import 'admin-lte/node_modules/bootstrap/dist/js/bootstrap';
 import 'jquery-colorbox/jquery.colorbox';
 import Rails from '@rails/ujs';
 import '../dmemo';
