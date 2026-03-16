5a1d52fc4279745b6717956feecaedf39509bdc4
Takuma Ishikawa <nekketsuuu@cookpad.com>
2026-03-16 20:17:55 +0900

    load_defaults 8.0

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module Dmemo
 
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.2
+    config.load_defaults 8.0
 
     # Configuration for the application, engines, and railties goes here.
     #
